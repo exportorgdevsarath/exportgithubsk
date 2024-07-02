5f7dec5d2d66580229ade4a8a3758076092bb9b0
rsarathkish <131669079+rsarathkish@users.noreply.github.com>
2024-07-02 11:50:24 +0530

    Update CSVToExcelFunction.java

--- BackendApp/src/functions/java/csvtoexcel/src/main/java/com/vb/csvtoexcel/CSVToExcelFunction.java
+++ BackendApp/src/functions/java/csvtoexcel/src/main/java/com/vb/csvtoexcel/CSVToExcelFunction.java
@@ -34,21 +34,38 @@ public class CSVToExcelFunction implements HttpFunction {
 			csvToExcelConverter.convertCSVToExcel(bucketName, csvFileName, excelFileName);
 			response.setStatusCode(200, "CSV file successfully converted to Excel and written to Cloud Storage.");
 		} catch (FileConversionException e) {
-			System.out.println("Error processing CSV file: errorcode " +e.getErrorCode());
-			System.out.println("Error processing CSV file: error msg " +e.getMessage());
-			if (429 == e.getErrorCode()) {
-				response.setStatusCode(429, "Maximum Limit is reached");
-			} else if (1001 == e.getErrorCode()) {
-				response.setStatusCode(1001, "Maximum Limit is reached");
-			}  else if (1002 == e.getErrorCode()) {
-				response.setStatusCode(1002, "Maximum Limit is reached");
-			}  else {
-				response.setStatusCode(500, "Maximum Limit is reached");
-			}
+			setResponseExceptionError(response, e);
 		} catch (OutOfMemoryError outOfMemoryError) {
-			response.setStatusCode(1003, "OutOfMemoryError processing CSV file: " + outOfMemoryError.getMessage());
+			CloudLogger.logError("Error processing CSV file: errorcode " +outOfMemoryError.getMessage());
+			response.appendHeader("X-Failure-Code", "1003");
+			response.setStatusCode(500, "OutOfMemoryError processing CSV file: " + outOfMemoryError.getMessage());
 		} catch (Exception e) {
+			CloudLogger.logError("Error processing CSV file: errorcode " +e.getMessage());
+			response.appendHeader("X-Failure-Code", "500");
 			response.setStatusCode(500, "Error processing CSV file: " + e.getMessage());
 		}
+	}
+	
+	private void setResponseExceptionError(com.google.cloud.functions.HttpResponse response, FileConversionException e) {
+		switch (e.getErrorCode()) {
+			case 429:
+				response.appendHeader("X-Failure-Code", "429");
+				response.setStatusCode(429, "The export reached the maximum number of rows in an excel sheet");
+				break;
+			case 1001:
+				response.appendHeader("X-Failure-Code", "1001");
+				response.setStatusCode(503, "Export excel conversion failed with Interuption exception");
+				break;
+			case 1002:
+				response.appendHeader("X-Failure-Code", "1002");
+				response.setStatusCode(500, "Export excel conversion failed with Excel write IO exception");
+				break;
+			default:
+				response.appendHeader("X-Failure-Code", "500");
+				response.setStatusCode(500, "Cloud Function execution has failed");
+				break;
+		}
+		CloudLogger.logError("Error processing CSV file: errorcode " +e.getErrorCode());
+		CloudLogger.logError("Error processing CSV file: error msg " +e.getMessage());
 	}
 }
